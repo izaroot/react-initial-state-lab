a84d6c519a1e859dc606e3c0700d6f3802fcc009
Isaac <izaroot327@gmail.com>
2021-04-27 14:04:23 -0400

    Completed Assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component{
+
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    decrement = () => {
+        // const newSecondsLeft = this.state.secondsLeft - 10
+        this.setState({
+            secondsLeft: this.state.secondsLeft - 10
+        })
+    }
+    render(){
+        return (
+            <div onClick={this.decrement}>
+                {this.state.secondsLeft !== 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : `Boom!` } 
+            </div>
+        )
+    }
+}
